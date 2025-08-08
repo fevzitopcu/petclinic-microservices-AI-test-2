cf58a4f5eaf67c5c94ce487c8c05603e8b08153a
fevzitopcu <fevzi@gmail.com>
2025-08-08 16:28:18 +0000

    ok

--- .devops/jenkins/2nd-pipeline-prod-deploy-app-on-EKS.sh
+++ .devops/jenkins/2nd-pipeline-prod-deploy-app-on-EKS.sh
@@ -17,12 +17,12 @@ envsubst < .devops/k8s/petclinic_chart/values-template.yaml > .devops/k8s/petcli
 
 sed -i "s/^version:.*/version: ${BUILD_NUMBER}/" .devops/k8s/petclinic_chart/Chart.yaml
 
-AWS_REGION=$AWS_REGION helm repo add stable-petclinic s3://petclinic-charts-fevzitopcu/stable/myapp/ || echo "repository name already exists"
+helm repo add stable-petclinic s3://petclinic-charts-fevzitopcu/stable/myapp/ || echo "repository name already exists"
 
-AWS_REGION=$AWS_REGION helm repo update
+helm repo update
 helm package .devops/k8s/petclinic_chart
 
-AWS_REGION=$AWS_REGION helm s3 push --force petclinic_chart-${BUILD_NUMBER}.tgz stable-petclinic
+helm s3 push --force petclinic_chart-${BUILD_NUMBER}.tgz stable-petclinic
 
 kubectl create ns petclinic-prod || echo "namespace petclinic-prod already exists"
 kubectl delete secret regcred -n petclinic-prod || echo "there is no regcred secret in petclinic-prod namespace"
