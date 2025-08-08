2bbb93908cc1f8c5ec920730777c56fa4441083a
fevzitopcu <fevzi@gmail.com>
2025-08-08 13:59:42 +0000

    ok

--- .devops/jenkins/2nd-pipeline-prod-deploy-app-on-EKS.sh
+++ .devops/jenkins/2nd-pipeline-prod-deploy-app-on-EKS.sh
@@ -2,7 +2,8 @@
 echo 'Deploying App on EKS K8s Cluster'
 envsubst < .devops/k8s/petclinic_chart/values-template.yaml > .devops/k8s/petclinic_chart/values.yaml
 
-sed -i \"s/^version:.*/version: ${BUILD_NUMBER}/\" .devops/k8s/petclinic_chart/Chart.yaml
+
+sed -i "s/^version:.*/version: ${BUILD_NUMBER}/" .devops/k8s/petclinic_chart/Chart.yaml
 
 AWS_REGION=$AWS_REGION helm repo add stable-petclinic s3://petclinic-charts-fevzitopcu/stable/myapp/ || echo "repository name already exists"
 
